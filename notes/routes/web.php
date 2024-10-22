f1132cf4e86ef2932e44f9f1a1809e6255a87641
Leonardo Rosario <leorosario9605@gmail.com>
2024-10-22 17:53:13 -0300

    Limpando o código

--- notes/routes/web.php
+++ notes/routes/web.php
@@ -3,12 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "Hello world!";
-});
-
-Route::get('/about', function () {
-    echo "About us";
-});
-
-Route::get('/main/{value}', [MainController::class, "index"]);
