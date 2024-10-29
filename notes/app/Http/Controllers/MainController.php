fa5c8b6d5c07ce59e7731eb0734b1338f77cf9c4
Leonardo Rosario <leorosario9605@gmail.com>
2024-10-29 17:51:27 -0300

    Adicionado criação de nota

--- notes/app/Http/Controllers/MainController.php
+++ notes/app/Http/Controllers/MainController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
+use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
@@ -20,30 +22,56 @@ class MainController extends Controller
 
     public function newNote()
     {
-        echo "I'm creating a new note!";
+        //Show new note view
+        return view("new_note");
+    }
+
+    public function newNoteSubmit(Request $request)
+    {
+        //validate request
+        $request->validate(
+            //rules
+            [
+                "text_title" => "required|min:3|max:200",
+                "text_note" => "required|min:3|max:3000"
+            ],
+            //error messages
+            [
+                "text_title.required" => "O título é obrigatório",
+                "text_title.min" => "O título deve ter pelo menos :min caracteres",
+                "text_title.max" => "O título deve ter no máximo :max caracteres",                              
+                "text_note.required" => "A nota é obrigatório",
+                "text_note.min" => "A nota deve ter pelo menos :min caracteres",
+                "text_note.max" => "A nota deve ter no máximo :max caracteres"
+            ]
+        );
+
+        
+        //get user id
+        $id = session("user.id");
+
+        //create new note
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->text_title;
+        $note->text = $request->text_note;
+        $note->save();
+
+        //redirect to home
+        return redirect()->route("home");
     }
 
     public function editNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);            
-        } catch (DecryptException $e) {
-            return redirect()->route("home");
-        }
-
-        echo "I'm editing note with id = $id";
-        
+        //$id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
+        echo "I'm editing note with id = $id";        
     }
 
     public function deleteNote($id)
     {
-        try {
-            $id = Crypt::decrypt($id);            
-        } catch (DecryptException $e) {
-            return redirect()->route("home");
-        }
-
-        echo "I'm deleting note with id = $id";
-        
-    }
+        //$id = $this->decryptId($id);
+        $id = Operations::decryptId($id);
+        echo "I'm deleting note with id = $id";        
+    } 
 }
