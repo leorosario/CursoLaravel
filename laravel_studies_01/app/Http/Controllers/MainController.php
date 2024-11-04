c59121887bad2ef842efadf42de3feecea528121
Leonardo Rosario <leorosario9605@gmail.com>
2024-11-04 18:01:04 -0300

    Criando rotas

--- laravel_studies_01/app/Http/Controllers/MainController.php
+++ laravel_studies_01/app/Http/Controllers/MainController.php
@@ -15,4 +15,34 @@ class MainController extends Controller
     {
         echo "about";
     }
+
+    public function mostrarValor($valor)
+    {
+        echo "Valor enviado pela rota: $valor";
+    }
+
+    public function mostrarValores($valor1, $valor2)
+    {
+        echo "Valores enviadso pela rota: $valor1 e $valor2";
+    }
+
+    public function mostrarValores2(Request $request, $valor1, $valor2)
+    {
+        echo "Valores enviadso pela rota: $valor1 e $valor2";
+    }
+
+    public function mostrarValorOpcional($valor = null)
+    {
+        echo "Valor opcional: $valor";
+    }
+
+    public function mostrarValorOpcional2($valor1, $valor2 = 100)
+    {
+        echo "Valor opcional: $valor1 e $valor2";
+    }
+
+    public function mostrarPosts($user_id, $post_id)
+    {
+        echo "Posts do usuário ID: $user_id e o Post com ID $post_id";
+    }
 }
--- laravel_studies_01/routes/web.php
+++ laravel_studies_01/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\OnlyAdmin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 //rota com função anônima
@@ -30,3 +33,80 @@ Route::permanentRedirect("/saltar2", "index");
 Route::view("/view", "home");
 
 Route::view("/view2", "home", ["myName" => "João Ribeiro"]);
+
+// ---------------------------------------
+// ROUTE PARAMETERS
+// ---------------------------------------
+
+Route::get("/valor/{value}", [MainController::class, "mostrarValor"]);
+Route::get("/valores/{value1}/{value2}", [MainController::class, "mostrarValores"]);
+Route::get("/valores2/{value1}/{value2}", [MainController::class, "mostrarValores2"]);
+
+Route::get("/opcional/{value?}", [MainController::class, "mostrarValorOpcional"]);
+Route::get("/opcional1/{value1}/{value2?}", [MainController::class, "mostrarValorOpcional2"]);
+
+Route::get("/user/{user_id}/post/{post_id}", [MainController::class, "mostrarPosts"]);
+
+// ---------------------------------------
+// ROUTE PARAMETERS WITH CONSTRAINTS
+// ---------------------------------------
+
+Route::get("/exp1/{value}", function($value){
+    echo $value;
+})->where("value", "[0-9]+");
+
+Route::get("/exp2/{value}", function($value){
+    echo $value;
+})->where("value", "[A-Za-z0-9]+");
+
+Route::get("/exp3/{value1}/{value2}", function($value1, $value2){
+    echo "$value1 : $value2";
+})->where([
+    "value1" => "[0-9]+",
+    "value2" => "[A-Za-z0-9]+"
+]);
+
+// ---------------------------------------
+// ROUTE NAMES
+// ---------------------------------------
+
+Route::get("/rota_abc", function(){
+    return "Rota nomeada";
+})->name("rota_nomeada");
+
+Route::get("/rota_referenciada", function(){
+    return redirect()->route("rota_nomeada");
+});
+
+Route::prefix("admin")->group(function(){
+    Route::get("/home", [MainController::class, "index"]);
+    Route::get("/about", [MainController::class, "about"]);
+    Route::get("/management", [MainController::class, "mostrarValor"]);
+});
+
+Route::get("admin/only", function(){
+    echo "Apenas administradores!";
+})->middleware([OnlyAdmin::class]);
+
+Route::middleware(OnlyAdmin::class)->group(function(){
+    Route::get("/admin/only2", function(){
+        return "Apenas administradores 1";
+    });
+    Route::get("/admin/only3", function(){
+        return "Apenas administradores 2";
+    });
+});
+
+//Route::get("/new", [UserController::class, "new"]);
+//Route::get("/edit", [UserController::class, "edit"]);
+//Route::get("/delete", [UserController::class, "delete"]);
+
+Route::controller(UserController::class)->group(function(){
+    Route::get("/user/new", "new");
+    Route::get("/user/edit", "edit");
+    Route::get("/user/delete", "delete");
+});
+
+Route::fallback(function(){
+    echo "PÁGINA NÃO ENCONTRADA";
+});
