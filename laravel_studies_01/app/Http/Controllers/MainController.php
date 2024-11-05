dd8eaed1f74be07f3e5bd9e8a05b23216a8df198
Leonardo Rosario <leorosario9605@gmail.com>
2024-11-05 18:01:14 -0300

    Controllers

--- laravel_studies_01/app/Http/Controllers/MainController.php
+++ laravel_studies_01/app/Http/Controllers/MainController.php
@@ -2,47 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function initMethod(): string
     {
-        echo "index";
+        return "Hello World!";
     }
 
-    public function about()
+    public function viewPage(): View
     {
-        echo "about";
-    }
-
-    public function mostrarValor($valor)
-    {
-        echo "Valor enviado pela rota: $valor";
-    }
-
-    public function mostrarValores($valor1, $valor2)
-    {
-        echo "Valores enviadso pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValores2(Request $request, $valor1, $valor2)
-    {
-        echo "Valores enviadso pela rota: $valor1 e $valor2";
-    }
-
-    public function mostrarValorOpcional($valor = null)
-    {
-        echo "Valor opcional: $valor";
-    }
-
-    public function mostrarValorOpcional2($valor1, $valor2 = 100)
-    {
-        echo "Valor opcional: $valor1 e $valor2";
-    }
-
-    public function mostrarPosts($user_id, $post_id)
-    {
-        echo "Posts do usuário ID: $user_id e o Post com ID $post_id";
+        return view("home");
     }
 }
