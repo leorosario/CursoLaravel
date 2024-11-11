e32253f3ea0341202ffd92b54dae16dc229561fd
Leonardo Rosario <leorosario9605@gmail.com>
2024-11-11 18:27:28 -0300

    iniciando mathx

---
+++ mathx/app/Http/Controllers/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class MainController extends Controller
+{
+    public function home():View
+    {
+        return view("home");
+    }
+
+    public function generateExercises(Request $request)
+    {
+        echo "Gerar exercícios";
+    }
+
+    public function printExercises()
+    {
+        echo "Imprimir exercícios no navegador";
+    }
+
+    public function exportExercises()
+    {
+        echo "Exportar exercícios para um arquivo de texto";
+    }
+}
