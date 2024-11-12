102c138d222e108021b72311a669604a73790d23
Leonardo Rosario <leorosario9605@gmail.com>
2024-11-12 17:46:32 -0300

    iniciando studies 03

---
+++ laravel_studies_03/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main_layout')
+
+@section('page_title', 'Home Page')
+    
+@section('content')
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col card p-5 bg-secondary text-center">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum beatae vitae quas consequatur. Esse, nulla. Quod, ullam repudiandae quia saepe laborum asperiores commodi ut odit omnis molestias sequi, porro nam.
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ laravel_studies_03/routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/',function(){
+    echo "Blade Layouts";
+});
+
+Route::view("/home", "home");
