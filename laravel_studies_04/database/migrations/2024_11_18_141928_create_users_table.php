d0013a2442376d8c4d1604c248bd096c8fcdab65
Leonardo Rosario <leorosario9605@gmail.com>
2024-11-18 12:50:03 -0300

    alterações

---
+++ laravel_studies_04/database/migrations/2024_11_18_141928_create_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string("username", 100)->nullable();
+            $table->string("password", 200)->nullable();
+            $table->boolean("active")->default(true);
+            $table->timestamps(); // create columns: create_at and updated_at
+            $table->softDeletes(); //create column: deleted_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
--- laravel_studies_04/routes/web.php
+++ laravel_studies_04/routes/web.php
@@ -1,22 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/mysql',function(){
-    try {
-        DB::connection()->getPDO();
-        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
-    } catch (\Exception $e) {
-        die("Não foi possível conectar com a base de dados. Errro: " . $e->getMessage());
-    }
-});
-
-Route::get('/sqlite',function(){
-    try {
-        DB::connection()->getPDO();
-        echo "Conexão com a base de dados: " . DB::connection()->getDatabaseName();
-    } catch (\Exception $e) {
-        die("Não foi possível conectar com a base de dados. Errro: " . $e->getMessage());
-    }
+Route::get('/mysql_test',function(){  
+    DB::connection("mysql")->getPDO();
+    echo "OK";    
 });
