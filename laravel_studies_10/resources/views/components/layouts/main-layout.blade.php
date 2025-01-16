dad3db7c16343db5c124a04f8f14152bc0de1a0c
Leonardo Rosario <leorosario9605@gmail.com>
2025-01-16 09:13:33 -0300

    iniciando studies 10

---
+++ laravel_studies_10/resources/views/components/layouts/main-layout.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Gates</title>
+    <link rel="stylesheet" href="{{ asset("assets/bootstrap/bootstrap.min.css") }}">
+</head>
+<body>
+
+    <div class="container mt-3">
+        <p class="display-6">Laravel Gates</p>
+        <hr>
+
+        @auth
+            <p><span class="text-info me-3">{{ Auth::user()->username }}</span><a href="#"></a></p>
+        @else
+            <p class="opacity-75">Nenhum usuário logado</p>
+        @endauth
+    </div>
+  
+    {{ $slot }}
+    
+    <script src="{{ asset("assets/bootstrap/bootstrap.bundle.min.js") }}"></script>
+</body>
+</html>
---
+++ laravel_studies_10/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<x-layouts.main-layout>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="display-6">
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</x-layouts.main-layout>
