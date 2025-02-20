729ae9bd29bbb7c787d6decd8ff3815d463cf222
Leonardo Rosario <leorosario9605@gmail.com>
2025-02-20 15:54:54 -0300

    alterações

--- laravel_filesystem/routes/web.php
+++ laravel_filesystem/routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
-Route::view('/', "home");
+Route::get("/", function(){
+    $contents = Storage::disk("public")->get("teste.txt");
+    echo $contents;
+});
