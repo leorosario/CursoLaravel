921773938e92f55276b2031c6367c643b63e491a
Leonardo Rosario <leorosario9605@gmail.com>
2025-04-12 21:13:41 -0300

    alterações

--- laravel_cashier/app/Http/Controllers/MainController.php
+++ laravel_cashier/app/Http/Controllers/MainController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 
 class MainController extends Controller
 {
@@ -18,7 +19,76 @@ class MainController extends Controller
         $user = User::findOrFail($id);
         if($user){
             auth()->login($user);
-            echo "Logado com uscesso! <br>" . auth()->user()->name;
+            return redirect()->route("plans");
         }
+    }
+
+    public function logout()    
+    {
+        auth()->logout();
+        return redirect()->route('login');
+    }
+
+    public function plans()
+    {
+        $prices = [
+            "monthly" => Crypt::encryptString(env('STRIP_PRODUCT_ID') . "|" . env('STRIP_MONTHLY_PRICE_ID')),
+            "yearly" => Crypt::encryptString(env('STRIP_PRODUCT_ID') . "|" . env('STRIP_YEARLY_PRICE_ID')),
+            "longest" => Crypt::encryptString(env('STRIP_PRODUCT_ID') . "|" . env('STRIP_LONGEST_PRICE_ID'))
+        ];
+        return view('plans', compact('prices'));
+    }
+
+    public function planSelected($id)
+    {
+        // check if $id is valid
+        $plan = Crypt::decryptString($id);
+        if(!$plan){
+            return redirect()->route('plans');
+        }
+
+        $plan = explode("|", $plan);
+        $product_id = $plan[0];
+        $price_id = $plan[1];
+        
+        return auth()->user()
+            ->newSubscription($product_id, $price_id)
+            ->checkout([
+                'success_url' => route('subscription.success'),
+                'cancel_url' => route('plans')
+        ]);
+    }
+
+    public function subscriptionSuccess()
+    {
+        return view("subscription_success");
+    }
+
+    public function dashboard()
+    {
+        $data = [];
+
+        // check the expiration of subscription
+        $timestamp = auth()->user()->subscription(env('STRIP_PRODUCT_ID'))
+            ->asStripeSubscription()
+            ->current_period_end;
+        
+        $data['subscription_end'] = date('d/m/Y H:i:s', $timestamp);
+
+        // get invoices
+        $invoices = auth()->user()->invoices();
+        $data['invoices'] = $invoices;
+
+        return view("dashbord", $data);
+    }
+
+    public function invoiceDownload($id)
+    {
+        // return auth()->user()->downloadInvoice($id);
+
+        return auth()->user()->downloadInvoice($id, [
+            'vendor' => 'Minha Empresa',
+            'product' => 'Laravel Cashier Plano Subscrito'
+        ]);
     }
 }
--- laravel_cashier/routes/web.php
+++ laravel_cashier/routes/web.php
@@ -1,8 +1,33 @@
 <?php
 
 use App\Http\Controllers\MainController;
-use App\Models\User;
+use App\Http\Middleware\hasSubscription;
+use App\Http\Middleware\isGuest;
+use App\Http\Middleware\isUser;
+use App\Http\Middleware\noSubscription;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [MainController::class, 'loginPage'])->name('login');
-Route::get('/login/{id}', [MainController::class, 'loginSubmit'])->name('login.submit');
+Route::redirect('/', '/login');
+
+Route::middleware([isGuest::class])->group(function(){
+    Route::get('/login', [MainController::class, 'loginPage'])->name('login');
+    Route::get('/login/{id}', [MainController::class, 'loginSubmit'])->name('login.submit');
+});
+
+Route::middleware([isUser::class])->group(function(){
+    Route::get('/logout', [MainController::class, 'logout'])->name('logout');
+
+    Route::middleware([noSubscription::class])->group(function(){
+        Route::get('/plans', [MainController::class, 'plans'])->name('plans');
+        Route::get('/plan_selected/{id}', [MainController::class, 'planSelected'])->name('plan.selected');        
+    });
+
+    Route::middleware([hasSubscription::class])->group(function(){
+        Route::get('/subscription/success', [MainController::class, 'subscriptionSuccess'])->name('subscription.success');
+        Route::get('/dashboard', [MainController::class, 'dashboard'])->name('dashboard');
+        Route::get('/invoice/{id}', [MainController::class, 'invoiceDownload'])->name('invoice.download');
+    });   
+});
+
+
+
